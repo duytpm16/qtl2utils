8697ad70ee5de98af6f23cdb91c0dbf8564a8bd9
Duy Pham <duy.pham@jax.org>
2018-10-17 12:58:13 -0400

    new markers name

--- R/lod_drop_rng.R
+++ R/lod_drop_rng.R
@@ -1,13 +1,13 @@
 lod_drop_rng <- function(markers, scan1_output, max_chr, max_peak, hilit_drop){
-  
-    markers <- transform(merge(markers[,c('chr','pos')], scan1_output ,by=0,all=TRUE),
+
+    markers2 <- transform(merge(markers[,c('chr','pos')], scan1_output ,by=0,all=TRUE),
                           row.names=Row.names, Row.names=NULL)
-    
-    markers <- subset(markers, chr == max_chr)
+
+    markers2 <- subset(markers2, chr == max_chr)
     lod_drop <- max_peak - 1.5
-    
-    markers <- markers[markers[,ncol(markers)] >= lod_drop,]
-    
-    return(range(markers$pos))
-  
+
+    markers2 <- markers2[markers2[,ncol(markers)] >= lod_drop,]
+
+    return(range(markers2$pos))
+
 }
